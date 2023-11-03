6ad171899d85e869afeece7966f534308f235e22
nguyenduy <nguyenduy011201@gmail.com>
2023-11-03 14:02:12 +0700

    init page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import {HomeComponent} from "./pages/home/home.component";
+import {Page404Component} from "./pages/page404/page404.component";
+import {ProductComponent} from "./pages/product/product.component";
+import {UserComponent} from "./pages/user/user.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'user', component: UserComponent },
+  { path: '**', component: Page404Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { AuthComponent } from "./pages/auth/auth.component";
+import { HomeComponent } from './pages/home/home.component';
+import { Page404Component } from './pages/page404/page404.component';
+import { ProductComponent } from './pages/product/product.component';
+import { UserComponent } from './pages/user/user.component';
 
 @NgModule({
-  declarations: [AppComponent, AuthComponent],
+  declarations: [AppComponent, AuthComponent, HomeComponent, Page404Component, ProductComponent, UserComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
