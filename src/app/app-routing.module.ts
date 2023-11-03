348b4ee6c13ea44d4f359613b25820c14a68d58a
nguyenduy <nguyenduy011201@gmail.com>
2023-11-03 14:30:34 +0700

    update page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,25 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./pages/home/home.component";
 import {Page404Component} from "./pages/page404/page404.component";
 import {ProductComponent} from "./pages/product/product.component";
 import {UserComponent} from "./pages/user/user.component";
+import {LoginComponent} from "./pages/auth/login/login.component";
+import {RegisterComponent} from "./pages/auth/register/register.component";
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'product', component: ProductComponent },
-  { path: 'user', component: UserComponent },
-  { path: '**', component: Page404Component }
+  {path: '', component: HomeComponent},
+  {path: 'product', component: ProductComponent},
+  {path: 'user', component: UserComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'user', component: UserComponent},
+  {path: '**', component: Page404Component}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { AuthComponent } from "./pages/auth/auth.component";
-import { HomeComponent } from './pages/home/home.component';
-import { Page404Component } from './pages/page404/page404.component';
-import { ProductComponent } from './pages/product/product.component';
-import { UserComponent } from './pages/user/user.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {AppComponent} from "./app.component";
+import {HomeComponent} from './pages/home/home.component';
+import {Page404Component} from './pages/page404/page404.component';
+import {ProductComponent} from './pages/product/product.component';
+import {UserComponent} from './pages/user/user.component';
+import {LoginComponent} from './pages/auth/login/login.component';
+import {RegisterComponent} from './pages/auth/register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, AuthComponent, HomeComponent, Page404Component, ProductComponent, UserComponent],
+  declarations: [AppComponent, HomeComponent, Page404Component, ProductComponent, UserComponent, LoginComponent, RegisterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
